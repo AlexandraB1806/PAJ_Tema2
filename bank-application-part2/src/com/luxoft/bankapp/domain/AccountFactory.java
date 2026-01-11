f10c1c32aa098c8de085be5512fc0c3f808c7408
AlexandraB1806 <bobocu.alexandra@gmail.com>
2026-01-11 15:57:24 +0200

    ex1 --account factory

---
+++ bank-application-part2/src/com/luxoft/bankapp/domain/AccountFactory.java
@@ -0,0 +1,40 @@
+package com.luxoft.bankapp.domain;
+
+public class AccountFactory {
+
+	private AccountFactory() {
+
+	}
+
+	public static Account createSavingAccount(int id, double amount) {
+		return new SavingAccount(id, amount);
+	}
+
+	public static Account createSavingAccount(int id, double amount, Currency currency) {
+		return new SavingAccount(id, amount, currency);
+	}
+
+	public static Account createCheckingAccount(int id, double amount, double overdraft) {
+		return new CheckingAccount(id, amount, overdraft);
+	}
+
+	public static Account createCheckingAccount(int id, double amount, double overdraft, Currency currency) {
+		return new CheckingAccount(id, amount, overdraft, currency);
+	}
+
+	public static Account createAccount(int accountType, int id, double amount, double overdraft) {
+        return switch (accountType) {
+            case AbstractAccount.SAVING_ACCOUNT_TYPE -> createSavingAccount(id, amount);
+            case AbstractAccount.CHECKING_ACCOUNT_TYPE -> createCheckingAccount(id, amount, overdraft);
+            default -> throw new IllegalArgumentException("Invalid account type: " + accountType);
+        };
+	}
+
+	public static Account createAccount(int accountType, int id, double amount, double overdraft, Currency currency) {
+        return switch (accountType) {
+            case AbstractAccount.SAVING_ACCOUNT_TYPE -> createSavingAccount(id, amount, currency);
+            case AbstractAccount.CHECKING_ACCOUNT_TYPE -> createCheckingAccount(id, amount, overdraft, currency);
+            default -> throw new IllegalArgumentException("Invalid account type: " + accountType);
+        };
+	}
+}
---
+++ bank-application-part2/src/com/luxoft/bankapp/tests/TestAccountFactory.java
@@ -0,0 +1,129 @@
+package com.luxoft.bankapp.tests;
+
+import org.junit.Test;
+
+import com.luxoft.bankapp.domain.AbstractAccount;
+import com.luxoft.bankapp.domain.Account;
+import com.luxoft.bankapp.domain.AccountFactory;
+import com.luxoft.bankapp.domain.CheckingAccount;
+import com.luxoft.bankapp.domain.Currency;
+import com.luxoft.bankapp.domain.SavingAccount;
+
+import static org.junit.Assert.*;
+
+public class TestAccountFactory {
+	
+	@Test
+	public void testCreateSavingAccount() {
+		Account account = AccountFactory.createSavingAccount(1, 1000.0);
+		
+		assertNotNull(account);
+		assertTrue(account instanceof SavingAccount);
+		assertEquals(1, account.getId());
+		assertEquals(1000.0, account.getBalance(), 0.0);
+		assertEquals(AbstractAccount.SAVING_ACCOUNT_TYPE, ((AbstractAccount) account).getType());
+	}
+	
+	@Test
+	public void testCreateSavingAccountWithCurrency() {
+		Currency currency = new Currency("USD");
+
+		Account account = AccountFactory.createSavingAccount(2, 2000.0, currency);
+		assertNotNull(account);
+		assertTrue(account instanceof SavingAccount);
+
+		SavingAccount savingAccount = (SavingAccount) account;
+
+		assertEquals(2, savingAccount.getId());
+		assertEquals(2000.0, savingAccount.getBalance(), 0.0);
+		assertEquals(currency, savingAccount.getCurrency());
+		assertEquals(AbstractAccount.SAVING_ACCOUNT_TYPE, savingAccount.getType());
+	}
+	
+	@Test
+	public void testCreateCheckingAccount() {
+		Account account = AccountFactory.createCheckingAccount(3, 1500.0, 100.0);
+		
+		assertNotNull(account);
+		assertTrue(account instanceof CheckingAccount);
+		CheckingAccount checkingAccount = (CheckingAccount) account;
+		assertEquals(3, checkingAccount.getId());
+		assertEquals(1500.0, checkingAccount.getBalance(), 0.0);
+		assertEquals(100.0, checkingAccount.getOverdraft(), 0.0);
+		assertEquals(AbstractAccount.CHECKING_ACCOUNT_TYPE, checkingAccount.getType());
+	}
+	
+	@Test
+	public void testCreateCheckingAccountWithCurrency() {
+		Currency currency = new Currency("EUR");
+		Account account = AccountFactory.createCheckingAccount(4, 2500.0, 200.0, currency);
+		
+		assertNotNull(account);
+		assertTrue(account instanceof CheckingAccount);
+
+		CheckingAccount checkingAccount = (CheckingAccount) account;
+
+		assertEquals(4, checkingAccount.getId());
+		assertEquals(2500.0, checkingAccount.getBalance(), 0.0);
+		assertEquals(200.0, checkingAccount.getOverdraft(), 0.0);
+		assertEquals(currency, checkingAccount.getCurrency());
+		assertEquals(AbstractAccount.CHECKING_ACCOUNT_TYPE, checkingAccount.getType());
+	}
+	
+	@Test
+	public void testCreateAccountByTypeSaving() {
+		Account account = AccountFactory.createAccount(AbstractAccount.SAVING_ACCOUNT_TYPE, 5, 3000.0, 0.0);
+		
+		assertNotNull(account);
+		assertTrue(account instanceof SavingAccount);
+
+		assertEquals(5, account.getId());
+		assertEquals(3000.0, account.getBalance(), 0.0);
+	}
+	
+	@Test
+	public void testCreateAccountByTypeChecking() {
+		Account account = AccountFactory.createAccount(AbstractAccount.CHECKING_ACCOUNT_TYPE, 6, 4000.0, 150.0);
+		
+		assertNotNull(account);
+		assertTrue(account instanceof CheckingAccount);
+
+		CheckingAccount checkingAccount = (CheckingAccount) account;
+
+		assertEquals(6, checkingAccount.getId());
+		assertEquals(4000.0, checkingAccount.getBalance(), 0.0);
+		assertEquals(150.0, checkingAccount.getOverdraft(), 0.0);
+	}
+	
+	@Test
+	public void testCreateAccountByTypeWithCurrency() {
+		Currency currency = new Currency("GBP");
+		
+		Account savingAccount = AccountFactory.createAccount(
+				AbstractAccount.SAVING_ACCOUNT_TYPE, 7, 5000.0, 0.0, currency);
+
+		assertTrue(savingAccount instanceof SavingAccount);
+		assertEquals(currency, ((SavingAccount) savingAccount).getCurrency());
+		
+		Account checkingAccount = AccountFactory.createAccount(
+				AbstractAccount.CHECKING_ACCOUNT_TYPE, 8, 6000.0, 250.0, currency);
+
+		assertTrue(checkingAccount instanceof CheckingAccount);
+		assertEquals(currency, ((CheckingAccount) checkingAccount).getCurrency());
+		assertEquals(250.0, ((CheckingAccount) checkingAccount).getOverdraft(), 0.0);
+	}
+	
+	@Test
+	public void testFactoryMethodsReturnDifferentInstances() {
+		Account account1 = AccountFactory.createSavingAccount(11, 1000.0);
+		Account account2 = AccountFactory.createSavingAccount(12, 1000.0);
+		
+		assertNotNull("Account1 should not be null", account1);
+		assertNotNull("Account2 should not be null", account2);
+
+        assertNotSame("Accounts should be different instances", account1, account2);
+
+		assertEquals("Account1 id should be 11", 11, account1.getId());
+		assertEquals("Account2 id should be 12", 12, account2.getId());
+	}
+}
